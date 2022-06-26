2b7e0929a415ad7f380532c93bbbd63a7a73306f
Patrick Creighton <pcreighton429@gmail.com>
2022-06-25 17:57:07 -0700

    Add git to installed packages

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 # Run with admin priviledges
-choco install delta fzf ripgrep starship -y
+choco install delta fzf git ripgrep starship -y
 Install-Module git-aliases -Scope CurrentUser -Allowclobber
 Install-Module -Name PSFzf
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
