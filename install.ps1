de58a1498ba3dea2ded204298a8b27e159581cab
Patrick Creighton <pcreighton429@gmail.com>
2022-06-25 17:32:50 -0700

    Remove choco installation

--- install.ps1
+++ install.ps1
@@ -1,5 +1,4 @@
 # Run with admin priviledges
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install delta fzf ripgrep starship -y
 Install-Module git-aliases -Scope CurrentUser -Allowclobber
 Install-Module -Name PSFzf
