93375f0260c0815fface8d3bafaa6762f5bd7de5
Patrick Creighton <pcreighton429@gmail.com>
2022-12-13 12:56:16 -0800

    Update installed packages/modules and install additional packages

--- install.ps1
+++ install.ps1
@@ -1,5 +1,14 @@
 # Run with admin priviledges
-choco install delta fzf git ripgrep starship -y
+
+# Update installed packages
+choco upgrade all -y
+Update-Module
+
+# Install packages if they aren't already installed
+choco install delta fzf git neovim ripgrep starship -y
 Install-Module git-aliases -Scope CurrentUser -Allowclobber
 Install-Module -Name PSFzf
+Install-Module posh-git -Scope CurrentUser -Force -AllowClobber
+Install-Module -Name Terminal-Icons -Repository PSGallery
+
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
