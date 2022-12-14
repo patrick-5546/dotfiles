b25b32fc6dd34624724a30f656e70bf5d4d0f1cc
Patrick Creighton <pcreighton429@gmail.com>
2022-12-13 17:48:09 -0800

    Use vim instead of neovim

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ choco upgrade all -y
 Update-Module
 
 # Install packages if they aren't already installed
-choco install bat delta dust fd fzf git lf neovim ripgrep starship -y
+choco install bat delta dust fd fzf git lf ripgrep starship vim -y
 Install-Module git-aliases -Scope CurrentUser -Allowclobber
 Install-Module -Name PSFzf
 Install-Module posh-git -Scope CurrentUser -Force -AllowClobber
