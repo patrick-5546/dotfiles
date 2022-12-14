60e5f99b3e0a5c9d622d684a95fba71d88246049
Patrick Creighton <pcreighton429@gmail.com>
2022-12-13 17:30:08 -0800

    Add more software for windows

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ choco upgrade all -y
 Update-Module
 
 # Install packages if they aren't already installed
-choco install delta fzf git neovim ripgrep starship -y
+choco install bat delta dust fd fzf git lf neovim ripgrep starship -y
 Install-Module git-aliases -Scope CurrentUser -Allowclobber
 Install-Module -Name PSFzf
 Install-Module posh-git -Scope CurrentUser -Force -AllowClobber
