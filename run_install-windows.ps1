75c8f194820f6491b581ec3b068a5f51222b0432
Patrick Creighton <pcreighton429@gmail.com>
2022-12-14 03:33:06 -0800

    Use gsudo to elevate powershell script

--- run_install-windows.ps1
+++ run_install-windows.ps1
@@ -1,17 +1,17 @@
-#Requires -RunAsAdministrator
+gsudo {
+    Write-Host "# Update packages and modules"
+    choco upgrade all -y
+    Update-Module
 
-Write-Host "# Update packages and modules"
-choco upgrade all -y
-Update-Module
+    Write-Host "`n# Install packages"
+    choco install bat delta dust fd fzf git lf ripgrep starship vim -y
 
-Write-Host "# Install packages"
-choco install bat delta dust fd fzf git lf ripgrep starship vim -y
+    Write-Host "`n# Install modules"
+    Install-Module git-aliases -Scope CurrentUser -Allowclobber
+    Install-Module -Name PSFzf
+    Install-Module posh-git -Scope CurrentUser -AllowClobber
+    Install-Module -Name Terminal-Icons -Repository PSGallery
 
-Write-Host "# Install modules"
-Install-Module git-aliases -Scope CurrentUser -Allowclobber
-Install-Module -Name PSFzf
-Install-Module posh-git -Scope CurrentUser -AllowClobber
-Install-Module -Name Terminal-Icons -Repository PSGallery
-
-Write-Host "# Set execution policy"
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Write-Host "`n# Set execution policy"
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+}
