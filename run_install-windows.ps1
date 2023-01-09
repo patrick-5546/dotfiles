30d9ce883637236ad4c63165c124cffa11c19751
Patrick Creighton <pcreighton429@gmail.com>
2023-01-09 15:17:49 -0800

    Move upgrading choco packages before winget packages

--- run_install-windows.ps1
+++ run_install-windows.ps1
@@ -1,3 +1,22 @@
+Write-Host "Request administrator access"
+gsudo {
+    Write-Host "Update choco packages and modules"
+    choco upgrade all -y
+    Update-Module
+
+    Write-Host "`nInstall choco packages"
+    choco install bat delta dust fd fzf lf ripgrep starship -y
+
+    Write-Host "`nInstall modules"
+    Install-Module git-aliases -Scope CurrentUser -Allowclobber
+    Install-Module -Name PSFzf
+    Install-Module posh-git -Scope CurrentUser -AllowClobber
+    Install-Module -Name Terminal-Icons -Repository PSGallery
+
+    Write-Host "`nSet execution policy"
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+}
+
 Write-Host "Update winget packages"
 winget upgrade --all
 Write-Host("")
@@ -28,22 +47,3 @@ wingetInstallIfNotInstalled("JAMSoftware.TreeSize.Free")
 wingetInstallIfNotInstalled("vim.vim")
 wingetInstallIfNotInstalled("Microsoft.VisualStudioCode")
 wingetInstallIfNotInstalled("Zoom.Zoom")
-
-Write-Host "Request administrator access"
-gsudo {
-    Write-Host "Update choco packages and modules"
-    choco upgrade all -y
-    Update-Module
-
-    Write-Host "`nInstall choco packages"
-    choco install bat delta dust fd fzf lf ripgrep starship -y
-
-    Write-Host "`nInstall modules"
-    Install-Module git-aliases -Scope CurrentUser -Allowclobber
-    Install-Module -Name PSFzf
-    Install-Module posh-git -Scope CurrentUser -AllowClobber
-    Install-Module -Name Terminal-Icons -Repository PSGallery
-
-    Write-Host "`nSet execution policy"
-    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-}
