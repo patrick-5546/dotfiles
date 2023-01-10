2e57d5c8bf066cf67e45724ec91bb17ff0760a3c
Patrick Creighton <pcreighton429@gmail.com>
2023-01-10 00:05:18 -0800

    Add which command in Windows

--- Documents/PowerShell/aliases.ps1
+++ Documents/PowerShell/aliases.ps1
@@ -32,3 +32,8 @@ function lfcd {
 function ssh-copy-id($remote) {
     cat ~/.ssh/id_rsa.pub | ssh $remote "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
 }
+
+function which($name)
+{
+        Get-Command $name | Select-Object -ExpandProperty Definition
+}
