4cc5ced5d3eef8d52edd5f355cfbf8c2501a399f
Patrick Creighton <pcreighton429@gmail.com>
2023-04-04 20:51:09 -0700

    Map <leader>sx and <C-c>

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- resume telescope search
+vim.keymap.set(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume" }
+)
+
+-- can use <C-c> to save edits to multiple rows
+vim.keymap.set("i", "<C-c>", "<Esc>")
