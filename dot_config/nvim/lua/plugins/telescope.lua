50927869797abaf89051caad38819644ac950dcd
Patrick Creighton <pcreighton429@gmail.com>
2023-04-05 03:19:52 -0700

    Add references

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,8 @@
+-- https://github.com/LazyVim/LazyVim/discussions/338#discussioncomment-5109479
 local Util = require("lazyvim.util")
 return {
   {
+    -- https://github.com/nvim-telescope/telescope.nvim
     "telescope.nvim",
     keys = {
       -- mimic vs code quick open
