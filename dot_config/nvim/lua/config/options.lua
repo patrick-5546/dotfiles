7c157ea442a3f9336be95f86e42c0d55a6b90d25
Patrick Creighton <pcreighton429@gmail.com>
2023-04-09 23:42:19 -0700

    Show both absolute and relative line numbers

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Show both absolute and relative line numbers
+vim.opt.statuscolumn = "%l %r"
