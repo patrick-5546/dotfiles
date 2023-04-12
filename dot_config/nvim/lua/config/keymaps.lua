aec0a6acd24190460eeb9ef078eab0ad7e4ce6d9
Patrick Creighton <pcreighton429@gmail.com>
2023-04-12 13:24:03 -0700

    Do not copy to clipboard by default

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -20,3 +20,31 @@ vim.cmd([[
   noremap <silent> <C-l> :<C-U>TmuxNavigateRight<cr>
   noremap <silent> <C-\> :<C-U>TmuxNavigatePrevious<cr>
 ]])
+
+-- Shortcut to use blackhole register by default
+vim.keymap.set("n", "d", '"_d', { noremap = true })
+vim.keymap.set("v", "d", '"_d', { noremap = true })
+vim.keymap.set("n", "D", '"_D', { noremap = true })
+vim.keymap.set("v", "D", '"_D', { noremap = true })
+vim.keymap.set("n", "c", '"_c', { noremap = true })
+vim.keymap.set("v", "c", '"_c', { noremap = true })
+vim.keymap.set("n", "C", '"_C', { noremap = true })
+vim.keymap.set("v", "C", '"_C', { noremap = true })
+vim.keymap.set("n", "x", '"_x', { noremap = true })
+vim.keymap.set("v", "x", '"_x', { noremap = true })
+vim.keymap.set("n", "X", '"_X', { noremap = true })
+vim.keymap.set("v", "X", '"_X', { noremap = true })
+
+-- Shortcut to use clipboard with <leader>
+vim.keymap.set("n", "<leader>d", "d", { noremap = true })
+vim.keymap.set("v", "<leader>d", "d", { noremap = true })
+vim.keymap.set("n", "<leader>D", "D", { noremap = true })
+vim.keymap.set("v", "<leader>D", "D", { noremap = true })
+vim.keymap.set("n", "<leader>c", "c", { noremap = true })
+vim.keymap.set("v", "<leader>c", "c", { noremap = true })
+vim.keymap.set("n", "<leader>C", "C", { noremap = true })
+vim.keymap.set("v", "<leader>C", "C", { noremap = true })
+vim.keymap.set("n", "<leader>x", "x", { noremap = true })
+vim.keymap.set("v", "<leader>x", "x", { noremap = true })
+vim.keymap.set("n", "<leader>X", "X", { noremap = true })
+vim.keymap.set("v", "<leader>X", "X", { noremap = true })
