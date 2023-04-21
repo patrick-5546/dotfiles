8e92077c78ef12dd428591ad351be1f34c0e6b87
Patrick Creighton <pcreighton429@gmail.com>
2023-04-20 17:33:34 -0700

    nvim: set default notification background

---
+++ dot_config/nvim/lua/plugins/nvim-notify.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    -- https://github.com/rcarriga/nvim-notify
+    "rcarriga/nvim-notify",
+    opts = {
+      -- set default background for when background is transparent
+      background_colour = "#000000",
+    },
+  },
+}
