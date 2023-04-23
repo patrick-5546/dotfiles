5c689c9f6c18ef2a7aae23e7e18215e8acd5a34f
Patrick Creighton <pcreighton429@gmail.com>
2023-04-23 16:54:22 -0700

    nvim: show hidden files when searching files

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -5,8 +5,12 @@ return {
     -- https://github.com/nvim-telescope/telescope.nvim
     "telescope.nvim",
     keys = {
+      -- show hidden files when searching files
+      { "<leader><space>", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
+      { "<leader>ff", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
+      { "<leader>fF", Util.telescope("files", { hidden = true, cwd = false }), desc = "Find Files (cwd)" },
       -- mimic vs code quick open
-      { "<C-p>", Util.telescope("find_files") },
+      { "<C-p>", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
     },
   },
 }
