ed102e6d9aa8f3428179a67d5dec08587cde4333
Patrick Creighton <pcreighton429@gmail.com>
2023-04-23 01:39:17 -0700

    nvim: use darker variant of theme

--- dot_config/nvim/lua/plugins/tokyonight.lua
+++ dot_config/nvim/lua/plugins/tokyonight.lua
@@ -3,6 +3,8 @@ return {
     -- https://github.com/folke/tokyonight.nvim
     "folke/tokyonight.nvim",
     opts = {
+      style = "night",
+
       -- https://www.lazyvim.org/configuration/recipes#make-tokyonight-transparent
       transparent = true,
       styles = {
