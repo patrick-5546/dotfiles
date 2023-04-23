68471eaed9d7f50cdad3c08fb48a72c5d0b4e1e1
Patrick Creighton <pcreighton429@gmail.com>
2023-04-23 01:39:46 -0700

    nvim: add signs to status column

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Show both absolute and relative line numbers
-vim.opt.statuscolumn = "%l %r"
+-- Show signs, absolute line numbers, and relative line numbers
+vim.opt.statuscolumn = "%s%l %r"
