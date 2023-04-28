ae0fbee8716d7c07ba119951ce7dda42466a7495
Patrick Creighton <pcreighton429@gmail.com>
2023-04-28 00:25:51 -0700

    nvim: only automatically install frequently used language parsers

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,32 @@ return {
     -- https://github.com/nvim-treesitter/nvim-treesitter
     "nvim-treesitter/nvim-treesitter",
     opts = {
-      ensure_installed = "all",
+      ensure_installed = {
+        -- defaults: https://www.lazyvim.org/plugins/treesitter#nvim-treesitter
+        "bash",
+        "c",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+
+        -- additional
+        "dockerfile",
+        "rust",
+        "verilog",
+      },
     },
   },
 }
