1bbdaed15df65d18122aaaf97ff0dfbefa626a41
Patrick Creighton <pcreighton429@gmail.com>
2024-01-02 11:32:28 -0800

    windows: update powershell config accordingly

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,11 +3,11 @@
 
 # Imports
 # - Slowest imports are posh-git, PsFzf, Terminal-Icons
-Import-Module git-aliases -DisableNameChecking
+# Import-Module git-aliases -DisableNameChecking
 #Import-Module posh-git
 # must import PSReadLine before PSFzf
-Import-Module PSReadLine
-Import-Module PsFzf
+# Import-Module PSReadLine
+# Import-Module PsFzf
 #Import-Module Terminal-Icons
 
 $ProfilePath=Split-Path -parent $profile
@@ -16,28 +16,28 @@ $ProfilePath=Split-Path -parent $profile
 . $ProfilePath/functions.ps1
 . $ProfilePath/ssh.autocomplete.ps1
 
-# PSReadLine configuration
-Set-PSReadLineOption -EditMode Vi
-# Shows navigable menu of all options when hitting Tab
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+## PSReadLine configuration
+#Set-PSReadLineOption -EditMode Vi
+## Shows navigable menu of all options when hitting Tab
+#Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
-# PsFzf configuration
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-$env:FZF_DEFAULT_OPTS = '--height 40% --layout=reverse --info=inline'
-# https://github.com/junegunn/fzf/wiki/Windows#relative-filepaths
-$env:FZF_DEFAULT_COMMAND = 'rg --files . 2> nul'
+## PsFzf configuration
+#Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+#$env:FZF_DEFAULT_OPTS = '--height 40% --layout=reverse --info=inline'
+## https://github.com/junegunn/fzf/wiki/Windows#relative-filepaths
+#$env:FZF_DEFAULT_COMMAND = 'rg --files . 2> nul'
 
-# Zoxide: https://github.com/ajeetdsouza/Zoxide
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell | Out-String)
-})
+## Zoxide: https://github.com/ajeetdsouza/Zoxide
+#Invoke-Expression (& {
+#    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+#    (zoxide init --hook $hook powershell | Out-String)
+#})
 
 # Aliases
 . $ProfilePath/aliases.ps1
 
-# Prompt
-Invoke-Expression (&starship init powershell)
+## Prompt
+#Invoke-Expression (&starship init powershell)
 
 #}
 #Write-Host "$PSCommandPath execution time: $executionTime"
--- Documents/PowerShell/aliases.ps1
+++ Documents/PowerShell/aliases.ps1
@@ -1,6 +1,7 @@
 Set-Alias .. Set-LocationUp
 Set-Alias ... Set-LocationUp2
 Set-Alias c chezmoi
+Set-Alias cat 'C:\Program Files\Git\usr\bin\cat.exe'
 Set-Alias cls clear
 #Set-Alias e 'exit'
 Set-Alias l Get-ChildItemAll
@@ -14,20 +15,20 @@ function gswm {
     git switch main
 }
 
-# From https://github.com/gokcehan/lf/blob/master/etc/lfcd.ps1
-function lfcd {
-    $tmp = [System.IO.Path]::GetTempFileName()
-    lf -last-dir-path="$tmp" $args
-    if (Test-Path -PathType Leaf "$tmp") {
-        $dir = Get-Content "$tmp"
-        Remove-Item -Force "$tmp"
-        if (Test-Path -PathType Container "$dir") {
-            if ("$dir" -ne "$pwd") {
-                cd "$dir"
-            }
-        }
-    }
-}
+## From https://github.com/gokcehan/lf/blob/master/etc/lfcd.ps1
+#function lfcd {
+#    $tmp = [System.IO.Path]::GetTempFileName()
+#    lf -last-dir-path="$tmp" $args
+#    if (Test-Path -PathType Leaf "$tmp") {
+#        $dir = Get-Content "$tmp"
+#        Remove-Item -Force "$tmp"
+#        if (Test-Path -PathType Container "$dir") {
+#            if ("$dir" -ne "$pwd") {
+#                cd "$dir"
+#            }
+#        }
+#    }
+#}
 
 function ssh-copy-id($remote) {
     cat ~/.ssh/id_rsa.pub | ssh $remote "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
