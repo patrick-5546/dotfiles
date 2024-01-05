2c73ec136665912080f05ce348e6d3221e39885e
Patrick Creighton <pcreighton429@gmail.com>
2024-01-05 14:52:14 -0800

    Simplify

--- dot_config/nixpkgs/config.nix
+++ dot_config/nixpkgs/config.nix
@@ -1,16 +1,8 @@
 {
-  packageOverrides = pkgs: with pkgs; rec {
-    myProfile = writeText "my-profile" ''
-      export PATH=$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/sbin:/bin:/usr/sbin:/usr/bin
-      export MANPATH=$HOME/.nix-profile/share/man:/nix/var/nix/profiles/default/share/man:/usr/share/man
-    '';
+  packageOverrides = pkgs: with pkgs; {
     myPackages = pkgs.buildEnv {
       name = "my-packages";
       paths = [
-        (runCommand "profile" {} ''
-          mkdir -p $out/etc/profile.d
-          cp ${myProfile} $out/etc/profile.d/my-profile.sh
-        '')
         bat
         delta
         du-dust
@@ -25,8 +17,6 @@
         zoxide
         zsh
       ];
-      pathsToLink = [ "/share/man" "/share/doc" "/bin" "/etc" ];
-      extraOutputsToInstall = [ "man" "doc" ];
     };
   };
 }
