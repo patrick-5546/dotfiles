0b043c5c85d5bb9f57438931b37b030cebcac125
Patrick Creighton <pcreighton429@gmail.com>
2024-01-23 04:02:09 -0800

    powershell: update aliases

--- Documents/PowerShell/aliases.ps1
+++ Documents/PowerShell/aliases.ps1
@@ -3,14 +3,55 @@ Set-Alias ... Set-LocationUp2
 Set-Alias c chezmoi
 Set-Alias cat 'C:\Program Files\Git\usr\bin\cat.exe'
 Set-Alias cls clear
-#Set-Alias e 'exit'
+Set-Alias e Invoke-Exit
 Set-Alias g git
 Set-Alias l Get-ChildItemAll
 Set-Alias ll Get-ChildItemVisible
 Set-Alias ls Get-ChildItemSimple
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+# The aliases below already exist
 #Set-Alias md mkdir
-#Set-Alias rd rmdir  # works but md doesn't
+#Set-Alias rd rmdir
+
+function Invoke-Chezmoi {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$SubCommand,
+
+        [Parameter(Mandatory=$false)]
+        [string[]]$Arguments
+    )
+
+    & chezmoi $SubCommand @Arguments
+}
+
+function cad {
+    Invoke-Chezmoi -SubCommand add @args
+}
+
+function cap {
+    Invoke-Chezmoi -SubCommand apply @args
+}
+
+function ccd {
+    Invoke-Chezmoi -SubCommand cd @args
+}
+
+function cdi {
+    Invoke-Chezmoi -SubCommand diff @args
+}
+
+function cst {
+    Invoke-Chezmoi -SubCommand status @args
+}
+
+function cup {
+    Invoke-Chezmoi -SubCommand update @args
+}
+
+function Invoke-Exit {
+    exit
+}
 
 function gswm {
     git switch main
