886a652d0d8a6af571c8cb056936c5e127f5474a
Patrick Creighton <pcreighton429@gmail.com>
2024-05-25 18:08:59 -0700

    nvim: missed an lsp plugin config

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -3,35 +3,35 @@ return {
     -- https://github.com/stevearc/conform.nvim
     "stevearc/conform.nvim",
     -- enabled = false,
-    opts = {
-      formatters_by_ft = {
-        c = { "clang_format" },
-        cpp = { "clang_format" },
-        lua = { "stylua" },
-        markdown = { "markdownlint" },
-        python = { "black", "isort" },
-        sh = { "shfmt" },
-        yaml = { "yamlfmt" },
-      },
-
-      -- The options you set here will be merged with the builtin formatters.
-      -- You can also define any custom formatters here.
-      formatters = {
-        -- # Example of using dprint only when a dprint.json file is present
-        -- dprint = {
-        --   condition = function(ctx)
-        --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-        --   end,
-        -- },
-        --
-        -- # Example of using shfmt with extra args
-        -- shfmt = {
-        --   prepend_args = { "-i", "2", "-ci" },
-        -- },
-        isort = {
-          prepend_args = { "--profile", "black" },
-        },
-      },
-    },
+    -- opts = {
+    --   formatters_by_ft = {
+    --     c = { "clang_format" },
+    --     cpp = { "clang_format" },
+    --     lua = { "stylua" },
+    --     markdown = { "markdownlint" },
+    --     python = { "black", "isort" },
+    --     sh = { "shfmt" },
+    --     yaml = { "yamlfmt" },
+    --   },
+    --
+    --   -- The options you set here will be merged with the builtin formatters.
+    --   -- You can also define any custom formatters here.
+    --   formatters = {
+    --     -- # Example of using dprint only when a dprint.json file is present
+    --     -- dprint = {
+    --     --   condition = function(ctx)
+    --     --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+    --     --   end,
+    --     -- },
+    --     --
+    --     -- # Example of using shfmt with extra args
+    --     -- shfmt = {
+    --     --   prepend_args = { "-i", "2", "-ci" },
+    --     -- },
+    --     isort = {
+    --       prepend_args = { "--profile", "black" },
+    --     },
+    --   },
+    -- },
   },
 }
