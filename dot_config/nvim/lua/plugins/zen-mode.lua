4a21d40ad6fa0750c5966d91072e6646ad482cb3
Patrick Creighton <pcreighton429@gmail.com>
2024-05-26 16:16:54 -0700

    nvim: add zen mode

---
+++ dot_config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    keys = {
+      { "<leader>uz",  "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" },
+    },
+    opts = {
+      window = {
+        width = 0.9,
+      }
+    }
+  },
+}
