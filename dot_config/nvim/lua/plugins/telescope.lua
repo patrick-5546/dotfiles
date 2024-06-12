f9446b05453b655e149936ccb7fc596084a76fd5
Patrick Creighton <pcreighton429@gmail.com>
2024-06-12 12:18:03 -0700

    nvim: update plugins and deprecated functions

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,16 +1,15 @@
 -- https://github.com/LazyVim/LazyVim/discussions/338#discussioncomment-5109479
-local Util = require("lazyvim.util")
 return {
   {
     -- https://github.com/nvim-telescope/telescope.nvim
     "telescope.nvim",
     keys = {
       -- show hidden files when searching files
-      { "<leader><space>", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
-      { "<leader>ff", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
-      { "<leader>fF", Util.telescope("files", { hidden = true, cwd = false }), desc = "Find Files (cwd)" },
+      { "<leader><space>", LazyVim.pick("auto", { hidden = true }), desc = "Find Files (Root Dir)" },
+      { "<leader>ff", LazyVim.pick("auto", { hidden = true }), desc = "Find Files (Root Dir)" },
+      { "<leader>fF", LazyVim.pick("auto", { hidden = true, root = false }), desc = "Find Files (cwd)" },
       -- mimic vs code quick open
-      { "<C-p>", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
+      { "<C-p>", LazyVim.pick("auto", { hidden = true }), desc = "Find Files (Root Dir)" },
     },
     opts = {
       defaults = {
