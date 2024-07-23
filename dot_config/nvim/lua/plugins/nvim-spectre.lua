8c1c0692844d2ea7005f73f462323ea7874ee0c4
Patrick Creighton <pcreighton429@gmail.com>
2024-07-23 00:56:39 -0700

    nvim: delete spectre config, use grug instead

--- dot_config/nvim/lua/plugins/nvim-spectre.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    -- https://github.com/nvim-pack/nvim-spectre
-    "nvim-pack/nvim-spectre",
-    keys = {
-      { "<leader>sp", function() require("spectre").open_file_search() end, desc = "Replace in current file (Spectre)" },
-    },
-  },
-}
