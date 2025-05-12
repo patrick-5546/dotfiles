4788b1ec9c472c4ab60db205abdb83a084f5d77c
Patrick Creighton <pcreighton429@gmail.com>
2025-05-12 11:07:25 -0700

    nvim: add comment

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Additional cpp files
 vim.cmd("autocmd BufRead,BufNewFile *.llc_repl set filetype=cpp")
 vim.cmd("autocmd BufRead,BufNewFile *.bpred set filetype=cpp")
