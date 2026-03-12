7d8b6cee66a69c4a5c85091a9ba4ab5847393a70
Patrick Creighton <pcreighton429@gmail.com>
2026-03-12 13:55:52 -0700

    nvim: do not run chezmoi edit on file

---
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    -- https://github.com/xvzc/chezmoi.nvim
+    "xvzc/chezmoi.nvim",
+
+    -- disable init function that runs chezmoi edit on file enter
+    init = function() end,
+  },
+}
