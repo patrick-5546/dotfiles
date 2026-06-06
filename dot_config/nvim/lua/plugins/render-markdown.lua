6ceea1eea2a78dc56dcaf8549004de08bdef6e7a
Patrick Creighton <pcreighton429@gmail.com>
2026-06-05 21:27:46 -0700

    nvim: disable language border

---
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    -- https://github.com/MeanderingProgrammer/render-markdown.nvim
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        language_border = "",
+      },
+    },
+  },
+}
